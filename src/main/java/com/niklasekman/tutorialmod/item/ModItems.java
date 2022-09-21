da1b0b035c9a32ad58c087d67cead67992754983
Niklas Ekman <niklas@niklasekman.com>
2022-09-21 13:36:48 +0300

    Adding a new creative mode tab

--- src/main/java/com/niklasekman/tutorialmod/item/ModItems.java
+++ src/main/java/com/niklasekman/tutorialmod/item/ModItems.java
@@ -1,7 +1,6 @@
 package com.niklasekman.tutorialmod.item;
 
 import com.niklasekman.tutorialmod.TutorialMod;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,10 +12,10 @@ public class ModItems {
 
     // Zircon
     public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
     // Raw Zircon
     public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
